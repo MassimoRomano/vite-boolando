94730448bb10bf6de127bb0676dfd61bf700beba
MassimoRomano <romano93m@gmail.com>
2024-03-21 16:35:58 +0100

    add footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,8 +5,31 @@ export default {
 </script>
 
 <template>
+    <footer>
+        <div class="container d-flex justify-content-between align-items-center py-3 ">
+            <div>
+                <div class="text-light">Boolando s.r.l</div>
+                <ul class="text-light d-flex list-unstyled gap-3">
+                    <li>Informazioni legali</li>
+                    <li>Informativa sulla privacy</li>
+                    <li>Diritto di recesso</li>
+                </ul>
+            </div>
+            <div>
+                <div class="text-light">
+                    Trovaci anche su
+                </div>
+                <div class="d-flex text-light">
+                    <a href="" class="items_social"><i class="fa-brands fa-x-twitter"></i></a>
+                    <a href="" class="items_social"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="" class="items_social"><i class="fa-brands fa-square-instagram"></i></a>
+                    <a href="" class="items_social"><i class="fa-brands fa-youtube"></i></a>
+                    <a href="" class="items_social"><i class="fa-brands fa-pinterest"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
 
 </template>
 
-<style>
-</style>
+<style></style>
